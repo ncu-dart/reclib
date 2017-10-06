c950ab40f976142b274c37b6c04d590fe206ec7e
Hung-Hsuan Chen <hhchen@g.ncu.edu.tw>
2017-10-06 15:47:38 +0800

    Add classifier

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='reclib',
+      version='0.1',
+      description='A package for recommender systems based on latent factor models',
+      long_description='This is unique in that we exclusively include our Weighted-SVD model.',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      keywords='svd svd++ wsvd weighted-svd mf matrix-factorization',
+      url='https://github.com/ncu-dart/reclib',
+      author='Hung-Hsuan Chen',
+      author_email='hhchen@ncu.edu.tw',
+      license='MIT',
+      packages=['reclib'],
+      install_requires=[
+            'numpy',
+      ],
+      include_package_data=True,
+      zip_safe=False)
