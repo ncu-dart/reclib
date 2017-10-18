f81b7a735c3fe4746a1a53054df37f403db4151e
Hung-Hsuan Chen <hhchen1105@gmail.com>
2017-10-18 22:26:18 +0800

    Change data folder

--- reclib/load_data.py
+++ reclib/load_data.py
@@ -1,11 +1,11 @@
 # Hung-Hsuan Chen <hhchen1105@gmail.com>
 # Creation Date : 09-02-2017
-# Last Modified: Sat Oct  7 08:25:15 2017
+# Last Modified: Wed Oct 18 22:24:30 2017
 
 import pkg_resources
 
 resource_package = __name__
-rel_data_folder = '../data'
+data_folder = 'data'
 
 
 def load_data(filename):
@@ -20,10 +20,10 @@ def load_data(filename):
 
 
 def load_movielens_100k():
-    filename = pkg_resources.resource_filename(resource_package, '/'.join((rel_data_folder, 'ml-100k.dat')))
+    filename = pkg_resources.resource_filename(resource_package, '/'.join((data_folder, 'ml-100k.dat')))
     return load_data(filename)
 
 
 def load_filmtrust():
-    filename = pkg_resources.resource_filename(resource_package, '/'.join((rel_data_folder, 'filmtrust.dat')))
+    filename = pkg_resources.resource_filename(resource_package, '/'.join((data_folder, 'filmtrust.dat')))
     return load_data(filename)
