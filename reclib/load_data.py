e41d2f95cf13286467f1d31d30528e42f022e03c
Hung-Hsuan Chen <hhchen1105@gmail.com>
2018-02-08 11:57:41 +0800

    Add sep token as a parameter for load_data()

--- reclib/load_data.py
+++ reclib/load_data.py
@@ -1,6 +1,6 @@
 # Hung-Hsuan Chen <hhchen1105@gmail.com>
 # Creation Date : 09-02-2017
-# Last Modified: Wed Oct 18 22:24:30 2017
+# Last Modified: Thu 08 Feb 2018 11:57:17 AM CST
 
 import pkg_resources
 
@@ -8,13 +8,13 @@ resource_package = __name__
 data_folder = 'data'
 
 
-def load_data(filename):
+def load_data(filename, sep="\t"):
     X = []
     with open(filename) as f:
         for line in f:
             # TODO: what if the separator is not space-like symbols?
             # fix the issue
-            user, item, rating = line.strip().split()
+            user, item, rating = line.strip().split(sep)
             X.append((user, item, float(rating)))
     return X
 
